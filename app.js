90af790d6d9d4ec32b8ea54590057b3e38a4f61d
F4rr3LL <shadow_2pac@mail.ru>
2018-07-25 18:01:08 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const status = document.getElementById('status');
+const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+let name = 'Проходил мимо...';
+
+const ws = new WebSocket('ws://localhost:3000');
+
+
+
+function setStatus(value) {
+    status.innerHTML = value;
+}
+
+function printMessage(value) {
+    const li = document.createElement('li');
+
+    li.innerHTML = `<strong>${value.name}:</strong>-${value.message}`;
+    messages.appendChild(li);
+}
+
+form.addEventListener('submit', event => {
+	event.preventDefault();
+	console.log(input.value);
+	// ws.send(input.value);
+	let data = JSON.stringify ({
+		name: name,
+		message: input.value
+	});
+
+
+
+
+	ws.send(data);
+
+
+
+	if (input.value.indexOf('/name') + 1) {
+		name = input.value.replace('/name', '');
+		// console.log('ИМЯ:');
+		// console.log(name);
+		let notify = JSON.stringify ({
+			name: 'Бот Валера',
+			message: 'Новый пользователь: '+ name
+		});
+		ws.send(notify);
+
+	}
+
+
+	input.value = '';
+});
+
+ws.onopen = () => setStatus('ONLINE');
+ws.onclose = () => setStatus('DISCONNECTED');
+ws.onmessage = response => {
+	console.log(response);
+	let msgdata = JSON.parse(response.data);
+	console.log(msgdata);
+	// let output = `<strong>${msgdata.name}:</strong>-${msgdata.message}`;
+	printMessage(msgdata);
+};
